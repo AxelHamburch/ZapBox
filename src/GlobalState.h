16466ab7f0df728a297b1b6959ffe5c11ab39d2c
axelhamburch <axelhamburch@protonmail.com>
2026-02-20 17:00:24 +0100

    Change default theme to black-white and rebuild firmware binaries

--- src/GlobalState.h
+++ src/GlobalState.h
@@ -30,7 +30,7 @@ extern WifiConfig wifiConfig;
 
 struct DisplayConfig {
   String orientation = "h";  // "h" for horizontal, "v" for vertical
-  String theme = "zapbox";
+  String theme = "black-white";
 };
 
 extern DisplayConfig displayConfig;
