b8bac4a05d88df6cb9eae2f50b27952cef196b5e
axelhamburch <axelhamburch@protonmail.com>
2026-06-27 23:51:06 +0200

    fix(DisplayTouch): remove invalid escape sequences in fmtBlockHeight

--- src/DisplayTouch.cpp
+++ src/DisplayTouch.cpp
@@ -1372,8 +1372,8 @@ static String fmtBlockHeight(const String& raw) {
   char buf[8];
   snprintf(buf, sizeof(buf), "%07ld", raw.toInt());
   String s = "";
-  s += buf[0]; s += '\.';
-  s += buf[1]; s += buf[2]; s += buf[3]; s += '\.';
+  s += buf[0]; s += '.';
+  s += buf[1]; s += buf[2]; s += buf[3]; s += '.';
   s += buf[4]; s += buf[5]; s += buf[6];
   return s;
 }
